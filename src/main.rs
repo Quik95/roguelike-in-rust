8e8b26c79f84a3db8cb9ec1dc427eac4aaa883e1
Sebastian Bartoszewicz <quikstyletv@gmail.com>
2023-07-13 00:04:08 +0200

    Chapter 5

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,14 @@
-use components::{Player, Position, Renderable};
-use map::{draw_map, new_map_test, TileType};
+use components::*;
+use map::Map;
 use rltk::GameState;
 use specs::prelude::*;
-use specs_derive::Component;
-
-use crate::map::new_map_rooms_and_corridors;
+use visibility_system::VisibilitySystem;
 
 mod components;
 mod map;
 mod player;
 mod rect;
+mod visibility_system;
 
 pub struct State {
     ecs: World,
@@ -17,6 +16,8 @@ pub struct State {
 
 impl State {
     fn run_systems(&mut self) {
+        let mut vis = VisibilitySystem {};
+        vis.run_now(&self.ecs);
         self.ecs.maintain();
     }
 }
@@ -28,8 +29,7 @@ impl GameState for State {
         self.run_systems();
         Player::player_input(self, ctx);
 
-        let map = self.ecs.fetch::<Vec<TileType>>();
-        draw_map(&map, ctx);
+        Map::draw_map(&self.ecs, ctx);
 
         let position = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
@@ -51,20 +51,29 @@ fn main() -> rltk::BError {
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
     gs.ecs.register::<Player>();
+    gs.ecs.register::<Viewshed>();
 
-    let (rooms, map) = new_map_rooms_and_corridors();
+    let map = Map::new_map_rooms_and_corridors();
+    let (player_x, player_y) = map.rooms[0].center();
     gs.ecs.insert(map);
-    let (player_x, player_y) = rooms[0].center();
 
     gs.ecs
         .create_entity()
-        .with(Position { x: player_x, y: player_y })
+        .with(Position {
+            x: player_x,
+            y: player_y,
+        })
         .with(Renderable {
             glyph: rltk::to_cp437('@'),
             fg: rltk::RGB::named(rltk::YELLOW),
             bg: rltk::RGB::named(rltk::BLACK),
         })
         .with(Player {})
+        .with(Viewshed {
+            visible_tiles: Vec::new(),
+            range: 8,
+            dirty: true,
+        })
         .build();
 
     rltk::main_loop(context, gs)
