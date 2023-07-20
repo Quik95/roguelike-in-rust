5136ea7c91f02def0f777e5fa86a9ae8f87835d6
Sebastian Bartoszewicz <quikstyletv@gmail.com>
2023-07-20 19:55:25 +0200

    Chapter 4.3 - BSP Dungeon

--- src/map_builders/mod.rs
+++ src/map_builders/mod.rs
@@ -1,11 +1,14 @@
+use rltk::RandomNumberGenerator;
 use specs::World;
 
 use crate::components::Position;
 use crate::map::Map;
+use crate::map_builders::bsp_dungeon::BspDungeonBuilder;
 use crate::map_builders::simple_map::SimpleMapBuilder;
 
 mod simple_map;
 mod common;
+mod bsp_dungeon;
 
 pub trait MapBuilder {
     fn build_map(&mut self);
@@ -17,5 +20,11 @@ pub trait MapBuilder {
 }
 
 pub fn random_builder(new_depth: i32) -> Box<dyn MapBuilder> {
-    Box::new(SimpleMapBuilder::new(new_depth))
+    let mut rng = RandomNumberGenerator::new();
+    let builder = rng.roll_dice(1, 2);
+    match builder {
+        1 => Box::new(SimpleMapBuilder::new(new_depth)),
+        2 => Box::new(BspDungeonBuilder::new(new_depth)),
+        _ => unreachable!("Ups your forgot to add a builder.")
+    }
 }
