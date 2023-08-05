8ed6e618f26e6c31b65461052f12abb940c2bd25
Sebastian Bartoszewicz <quikstyletv@gmail.com>
2023-08-05 19:23:57 +0200

    Chapter 4.6 - Drunkard's Walk Maps

--- src/map_builders/cellular_automata.rs
+++ src/map_builders/cellular_automata.rs
@@ -1,13 +1,13 @@
-use std::collections::{hash_map, HashMap};
-use std::process::id;
-use std::thread::spawn;
-use rltk::{CellularDistanceFunction, DijkstraMap, FastNoise, NoiseType, RandomNumberGenerator};
+use std::collections::HashMap;
+
+use rltk::RandomNumberGenerator;
 use specs::World;
 
+use crate::{SHOW_MAPGEN_VISUALIZER, spawner};
 use crate::components::Position;
 use crate::map::{Map, TileType};
+use crate::map_builders::common::{generate_voronoi_spawn_regions, remove_unreachable_areas_returning_most_distant};
 use crate::map_builders::MapBuilder;
-use crate::{SHOW_MAPGEN_VISUALIZER, spawner};
 
 const MIN_ROOM_SIZE: i32 = 8;
 
@@ -16,7 +16,7 @@ pub struct CellularAutomataBuilder {
     starting_position: Position,
     depth: i32,
     history: Vec<Map>,
-    noise_areas: HashMap<i32, Vec<usize>>
+    noise_areas: HashMap<i32, Vec<usize>>,
 }
 
 impl MapBuilder for CellularAutomataBuilder {
@@ -60,7 +60,7 @@ impl CellularAutomataBuilder {
             starting_position: Position { x: 0, y: 0 },
             depth: new_depth,
             history: Vec::new(),
-            noise_areas: HashMap::new()
+            noise_areas: HashMap::new(),
         }
     }
 
@@ -105,49 +105,13 @@ impl CellularAutomataBuilder {
 
         self.starting_position = Position { x: self.map.width / 2, y: self.map.height / 2 };
         let mut start_idx = Map::xy_idx(self.starting_position.x, self.starting_position.y);
-        while self.map.tiles[start_idx] != TileType::Floor {
-            self.starting_position.x -= 1;
-            start_idx = Map::xy_idx(self.starting_position.x, self.starting_position.y);
-        }
 
-        let map_start = vec![start_idx];
-        let dijkstra_map = DijkstraMap::new(self.map.width, self.map.height, &map_start, &self.map, 200.0);
-        let mut exit_tile = (0, 0.0f32);
-        for (i, tile) in self.map.tiles.iter_mut().enumerate() {
-            if *tile == TileType::Floor {
-                let distance_to_start = dijkstra_map.map[i];
-                if distance_to_start == f32::MAX {
-                    *tile = TileType::Wall;
-                } else if distance_to_start > exit_tile.1   {
-                    exit_tile.0 = i;
-                    exit_tile.1 = distance_to_start;
-                }
-            }
-        }
+        let exit_tile = remove_unreachable_areas_returning_most_distant(&mut self.map, start_idx);
         self.take_snapshot();
 
-        self.map.tiles[exit_tile.0] = TileType::DownStairs;
+        self.map.tiles[exit_tile] = TileType::DownStairs;
         self.take_snapshot();
 
-        let mut noise = FastNoise::seeded(rng.roll_dice(1, 65536i32) as u64);
-        noise.set_noise_type(NoiseType::Cellular);
-        noise.set_frequency(0.08);
-        noise.set_cellular_distance_function(CellularDistanceFunction::Manhattan);
-
-        for y in 1..self.map.height-1 {
-            for x in 1..self.map.width-1 {
-                let idx = Map::xy_idx(x, y);
-                if self.map.tiles[idx] == TileType::Floor {
-                    let cell_value_f = noise.get_noise(x as f32, y as f32) * 10240.0;
-                    let cell_value = cell_value_f as i32;
-
-                    if let hash_map::Entry::Vacant(e) = self.noise_areas.entry(cell_value) {
-                        e.insert(vec![idx]);
-                    } else {
-                        self.noise_areas.get_mut(&cell_value).unwrap().push(idx);
-                    }
-                }
-            }
-        }
+        self.noise_areas = generate_voronoi_spawn_regions(&self.map, &mut rng);
     }
 }
